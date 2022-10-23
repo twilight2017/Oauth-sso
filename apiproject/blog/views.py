662ac02080783cfd9354f8714f72bebad0e03435
twilight2017 <liuzixuanp@163.com>
2022-10-23 19:44:15 +0800

    view based on function

--- apiproject/blog/views.py
+++ apiproject/blog/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from .models import Article
+from .serializers import ArticleSerializer
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def article_list(request, format=None):
+    """
+    List all articles, or create a new articel
+    :param request:
+    :return:
+    """
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            # Very important, Associate request.user with author
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def article_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete an article review
+    :param request:
+    :param pk:
+    :return:
+    """
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.methpd == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    elif request.methpd == 'PUT':
+        serializer = ArticleSerializer(article, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
